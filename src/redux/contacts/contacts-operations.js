33664bdf5f3f81a8fb7635ae8b20329b2781f3b6
Halyna Sinko <galinasinko14@gmail.com>
2021-09-15 13:21:32 +0300

    Update contacts-operations.js

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -1,45 +1,41 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 import * as contactsApi from "../../services/contacts-api";
 
-
-
 export const fetchContacts = createAsyncThunk(
-  'contacts/fetchContacts',
-  async (_,{ rejectWithValue }) => {
+  "contacts/fetchContacts",
+  async (_, { rejectWithValue }) => {
     try {
-      const { data } = await contactsApi.fetchAllContacts()
+      const { data } = await contactsApi.fetchAllContacts();
       return data;
     } catch (error) {
-      return rejectWithValue(error)
+      return rejectWithValue(error);
     }
   }
-)
+);
 
 export const addContact = createAsyncThunk(
-  'contacts/addContact',
+  "contacts/addContact",
   async ({ name, number }, { rejectWithValue }) => {
-  const contact = { name, number };
- try {
+    const contact = { name, number };
+    try {
       const { data } = await contactsApi.addNewContact(contact);
       console.log(data);
       return data;
     } catch (error) {
       return rejectWithValue(error);
-    } 
-})
-
+    }
+  }
+);
 
 export const removeContact = createAsyncThunk(
-  'contacts/removeContac',
+  "contacts/removeContac",
   async (contactId, { rejectWithValue }) => {
-   try {
-    await contactsApi.removeContactById(contactId);
-    return contactId;
-  } catch (error) {
-     return rejectWithValue(error);
+    try {
+      await contactsApi.removeContactById(contactId);
+      return contactId;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
   }
-})
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { removeContact };
+);
