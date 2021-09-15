e904af50db66a5a14e00eb70dd7d8eeb09167227
Halyna Sinko <galinasinko14@gmail.com>
2021-09-15 13:18:33 +0300

    change condition for render

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,27 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { authSelectors } from "../../redux/auth";
 import s from "./Navigation.module.css";
 
-const Navigation = () => (
-  <nav>
-    <NavLink to="/" exact className={s.link} activeClassName={s.activLink}>
-      Home
-    </NavLink>
-    <NavLink to="/contacts" exact className={s.link} activeClassName={s.activLink}>
-      Contacts
-    </NavLink>
-  </nav>
-);
+const Navigation = () => {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  return (
+    <nav>
+      <NavLink to="/" exact className={s.link} activeClassName={s.activLink}>
+        Home
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink
+          to="/contacts"
+          exact
+          className={s.link}
+          activeClassName={s.activLink}
+        >
+          Contacts
+        </NavLink>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
